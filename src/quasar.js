2d9baaa824512eb66026f19f5d65bb7b900f49e4
robertojmm <robertojmunozdeleon@gmail.com>
2020-04-18 23:11:59 +0200

    feat(quasar): Added "loading" to plugins

--- src/quasar.js
+++ src/quasar.js
@@ -1,17 +1,22 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import './styles/quasar.sass'
-import lang from 'quasar/lang/en-gb.js'
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-import { Quasar } from 'quasar'
+import "./styles/quasar.sass";
+import lang from "quasar/lang/en-gb.js";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import { Quasar, Loading } from "quasar";
 
 Vue.use(Quasar, {
   config: {},
-  components: { /* not needed if importStrategy is not 'manual' */ },
-  directives: { /* not needed if importStrategy is not 'manual' */ },
-  plugins: {
+  components: {
+    /* not needed if importStrategy is not 'manual' */
   },
-  lang: lang
- })
+  directives: {
+    /* not needed if importStrategy is not 'manual' */
+  },
+  plugins: {
+    Loading,
+  },
+  lang: lang,
+});
