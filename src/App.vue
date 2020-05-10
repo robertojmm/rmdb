076b798e5c66c191ce1cc1d857425483789da1ec
robertojmm <robertojmunozdeleon@gmail.com>
2020-05-10 20:41:15 +0200

    feat(app): Modified to just include the router and the drawer

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,80 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
+        <q-btn dense flat round icon="menu" @click="drawer = !drawer" />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title>Robert's Movie Database</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="drawer"
       show-if-above
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+      :width="200"
+      :breakpoint="500"
       bordered
-      content-class="bg-grey-2"
+      content-class="bg-grey-3"
     >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://github.com/quasarframework/"
-        >
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://chat.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://forum.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://twitter.com/quasarframework"
-        >
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item to="/add" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="add_circle"></q-icon>
+            </q-item-section>
+
+            <q-item-section>Add movie</q-item-section>
+          </q-item>
+
+          <q-item to="/library" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list"></q-icon>
+            </q-item-section>
+
+            <q-item-section>Movie library</q-item-section>
+          </q-item>
+
+          <q-separator></q-separator>
+
+          <q-item to="/settings" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="settings"></q-icon>
+            </q-item-section>
+
+            <q-item-section>Settings</q-item-section>
+          </q-item>
+
+          <q-item to="/about" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="info"></q-icon>
+            </q-item-section>
+
+            <q-item-section>About</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
-      <HelloWorld />
+      <!-- Is this legal?? (Cache) -->
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "LayoutDefault",
-
-  components: {
-    HelloWorld,
-  },
-
   data() {
     return {
-      leftDrawerOpen: false,
+      drawer: false,
+      miniState: true,
     };
   },
 };
 </script>
-
-<style></style>
