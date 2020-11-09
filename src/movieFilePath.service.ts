4c094fc667959f02543da5a53c1d4a006626609e
robertojmm <robertojmunozdeleon@gmail.com>
2020-11-09 18:13:07 +0000

    fix(movieFilePath): Error when editing a movie

--- src/movieFilePath.service.ts
+++ src/movieFilePath.service.ts
@@ -1,34 +1,37 @@
 import open from "open";
-import fs from "fs"
-import { settings } from './settings';
+import fs from "fs";
+import { settings } from "./settings";
 
 export function openByPath(path: string) {
-    const movieFolders = settings.get("directories").movies;
-    let error = true;
+  const movieFolders = settings.get("directories").movies;
+  let error = true;
 
-    for(const movieFolder of movieFolders) {
-        const fullPath = movieFolder + path
-        console.log(fullPath)
+  for (const movieFolder of movieFolders) {
+    const fullPath = movieFolder + path;
+    console.log(fullPath);
 
-        if(fs.existsSync(fullPath)) {
-            open(fullPath);
-            error = false;
-        }
+    if (fs.existsSync(fullPath)) {
+      open(fullPath);
+      error = false;
     }
+  }
 
-    return error;
+  return error;
 }
 
 export function checkPathIsCorrect(moviePath: string) {
-    const movieFolders = settings.get("directories").movies;
+  const movieFolders = settings.get("directories").movies;
 
-    for(const folder of movieFolders) {
-        const pathIsCorrect = moviePath.startsWith(folder);
+  for (const folder of movieFolders) {
+    const pathIsCorrect =
+      moviePath.startsWith(folder) ||
+      moviePath.startsWith("/") ||
+      moviePath.startsWith("\\");
 
-        if(pathIsCorrect) {
-            const newPath = moviePath.replace(folder, "");
-            return newPath;
-        }
+    if (pathIsCorrect) {
+      const newPath = moviePath.replace(folder, "");
+      return newPath;
     }
-    return "";
+  }
+  return "";
 }
